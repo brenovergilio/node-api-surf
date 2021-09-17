95907a93525855b011bd6743a10618c874283cbe
Breno Vergilio <bjvergilio47@gmail.com>
2021-09-17 14:23:10 -0400

    api errors good practices

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import logger from '@src/logger';
 import { CUSTOM_VALIDATION } from '@src/models/user';
+import ApiError, { APIError } from '@src/util/errors/api-errors';
 import { Response } from 'express';
 import mongoose from 'mongoose';
 
@@ -12,10 +13,17 @@ export abstract class BaseController {
       const clientErrors = this.handleClientErrors(error);
       res
         .status(clientErrors.code)
-        .send({ code: clientErrors.code, error: clientErrors.error });
+        .send(
+          ApiError.format({
+            code: clientErrors.code,
+            message: clientErrors.error,
+          })
+        );
     } else {
       logger.error(error);
-      res.status(500).send({ code: 500, error: 'Internal Server Error' });
+      res
+        .status(500)
+        .send(ApiError.format({ code: 500, message: 'Internal Server Error' }));
     }
   }
 
@@ -37,5 +45,9 @@ export abstract class BaseController {
       return { code: 409, error: error.message };
     }
     return { code: 422, error: error.message };
+  }
+
+  protected sendErrorResponse(res: Response, apiError: APIError): Response {
+    return res.status(apiError.code).send(ApiError.format(apiError));
   }
 }
