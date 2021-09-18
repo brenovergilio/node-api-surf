700ff432bd68feaeebd12d620182c4d52d68d41a
Breno Vergilio <bjvergilio47@gmail.com>
2021-09-17 23:03:06 -0400

    README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Node.js API com Typescript
+
+Desenvolvimento de uma API que calcula a melhor condição de surf entre várias praias. A API externa consultada é a [StormGlass](https://stormglass.io/).
+O projeto visa aprofundar conhecimentos sobre Typescript, TDD e boas práticas de desenvolvimento.
+
+## Principais tecnologias utilizadas
+
+💻 [Node.js](https://nodejs.org/)
+
+🧰 [Typescript](https://www.typescriptlang.org/)
+
+✅ [Jest](https://jestjs.io/)
+
+📦 [MongoDB](https://www.mongodb.com/)
--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,7 +1,5 @@
 import { Beach, GeoPosition } from '@src/models/beach';
 import nock from 'nock';
-import stormGlassWheater3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
-import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 import { User } from '@src/models/user';
 import AuthService from '@src/services/auth';
 
@@ -25,31 +23,6 @@ describe('Beach forecast functional tests', () => {
       user: user.id,
     };
     await new Beach(defaultBeach).save();
-  });
-
-  it('should return a forecast with just a few times', async () => {
-    nock('https://api.stormglass.io:443', {
-      encodedQueryParams: true,
-      reqheaders: {
-        Authorization: (): boolean => true,
-      },
-    })
-      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-      .get('/v2/weather/point')
-      .query({
-        lat: '-33.792726',
-        lng: '151.289824',
-        params: /(.*)/,
-        source: 'noaa',
-        end: /(.*)/,
-      })
-      .reply(200, stormGlassWheater3HoursFixture);
-
-    const { body, status } = await global.testRequest
-      .get('/forecast')
-      .set({ 'x-access-token': token });
-    expect(status).toBe(200);
-    expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
 
   it('should return 500 if something goes wrong during the processing', async () => {
